28d9ea56b1fb4a68d1cb475caf39c7483b9a092e
mozarcxiaye-dev <mozarcxiaye.dev@gmail.com>
2026-05-01 02:16:46 +0545

    Chore Assignment1 Week2: HTML and Logic created for the Vanilla Weather App

---
+++ week-2/assignment1/main.js
@@ -0,0 +1,46 @@
+
+
+// select the elements
+const cityInput = document.getElementById("city-input")
+const searchBtn = document.getElementById("search-btn")
+const weatherResult = document.getElementById("weather-result")
+
+
+
+// fetch the weather
+async function fetchWeather(city) {
+
+    const API_KEY = ""
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+    const data = await response.json()
+    return data
+}
+
+
+// render the weather
+function renderWeather(data) {
+
+    const iconCode = data.weather[0].icon;
+    const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@4x.png`;
+
+    weatherResult.innerHTML = `
+          <h2>${data.name}</h2>
+          <p>Temperature:${data.main.temp}</p>
+          <img src="${iconUrl}" alt="${data.weather[0].description}" />
+          <p>Condition:${data.weather[0].description}</p>
+          <p>Humidity:${data.main.humidity}</p>
+          <p>Sea Level: ${data.main.sea_level}</p>`
+}
+
+
+// event listener placeholderto the click button
+searchBtn.addEventListener("click", async () => {
+    const city = cityInput.value
+
+    if(city) {
+        const weather = await fetchWeather(city)
+        console.log(weather)
+        renderWeather(weather)
+    }
+})
