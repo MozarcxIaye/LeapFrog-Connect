3bcbaeff73e8560df6b3acd1fbd6af49c48cb008
mozarcxiaye-dev <mozarcxiaye.dev@gmail.com>
2026-05-01 17:14:28 +0545

    Feat: styles added, Exception handled, and minimal loading message added

--- week-2/assignment1/main.js
+++ week-2/assignment1/main.js
@@ -10,11 +10,22 @@ const weatherResult = document.getElementById("weather-result")
 // fetch the weather
 async function fetchWeather(city) {
 
-    const API_KEY = ""
+    try {
 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
-    const data = await response.json()
-    return data
+        if (!city) throw new Error("City not found")
+
+        const API_KEY = ""
+
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+
+        if (!response.ok) throw new Error("City not found")
+
+        const data = await response.json()
+        return data
+    } catch (error) {
+        console.log(error.message)
+    }
+
 }
 
 
@@ -26,11 +37,12 @@ function renderWeather(data) {
 
     weatherResult.innerHTML = `
           <h2>${data.name}</h2>
-          <p>Temperature:${data.main.temp}</p>
           <img src="${iconUrl}" alt="${data.weather[0].description}" />
-          <p>Condition:${data.weather[0].description}</p>
-          <p>Humidity:${data.main.humidity}</p>
-          <p>Sea Level: ${data.main.sea_level}</p>`
+          <p>Temperature: ${data.main.temp}°C</p>
+          <p>Condition: ${data.weather[0].description}</p>
+          <p>Humidity: ${data.main.humidity}%</p>
+          <p>Sea Level: ${data.main.sea_level}</p>
+          <p>Wind Speed: ${data.wind.speed} m/s</p>`
 }
 
 // for submitting the form
@@ -38,9 +50,21 @@ async function handleSubmit(e) {
 
     e.preventDefault()
     // to handle the from submission 
-    const city = cityInput.value
+    const city = cityInput.value.trim()
+    weatherResult.textContent = "Loading weather data..."
+    try {
+        if (!city) throw new Error("City not found")
 
-    if(!city) return 
-    const weather = await fetchWeather(city)
-    renderWeather(weather)
+        const weather = await fetchWeather(city)
+
+        if(!weather) throw new Error(`Could not find the weather data for the city ${city}`)
+
+        renderWeather(weather)
+        // console.log(weather)
+    } catch (error) {
+        weatherResult.textContent = error.message
+        console.error(error)
+
+        // console.log(error)
+    }
 }
