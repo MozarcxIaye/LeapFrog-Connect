bbbbabe040cfe0862464a1e166e743576d237695
Krish Shrestha <moz@Krishs-MacBook-Air.local>
2026-05-12 18:31:12 +0545

    Chore: Added DateTime utility function for formatted date and time.

---
+++ week-3/assignment2/src/utils/dateTime.js
@@ -0,0 +1,15 @@
+
+export function getFormattedDateTime() {
+    const now = new Date()
+
+    const date = new Intl.DateTimeFormat('en-Np', {
+        dateStyle: 'medium',
+    }).format(now)
+
+    const time = new Intl.DateTimeFormat('en-Np', {
+        timeStyle: "short"
+    }).format(now)
+
+    return {date, time}
+}
+
