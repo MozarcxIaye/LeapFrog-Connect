a3b10ccc0f88b0ef30cb38e899b47d7968928e38
Krish Shrestha <moz@Krishs-MacBook-Air.local>
2026-05-13 15:35:23 +0545

    Fix: Fixed Sidebar Styles and added upcomming meetings

--- week-3/assignment2/src/pages/Dashboard.jsx
+++ week-3/assignment2/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { DASHBOARD_ROUTES } from '../data/dashboardRoute'
 import "./Dashboard.css"
+import { UPCOMMING_MEETINGS } from '../data/upcommingMeetings'
 
 const Dashboard = () => {
   const user = "Chris"
@@ -19,10 +20,39 @@ const Dashboard = () => {
             return <div className='dashboard-cards'>
               <h2>{item.label}</h2>
               <p>{item.description}</p>
-              <button style={{background: item.variant}}>{item.buttontext}</button>
+              <button style={{ background: item.variant }}>{item.buttontext}</button>
             </div>
           })}
         </div>
+      </section>
+
+      <section className='upcomming-meetings'>
+        <div className="meetings-header">
+          <h2>Upcomming Meetings</h2>
+          <div>
+            <button>View All</button>
+          </div>
+        </div>
+        <div className="meetings">
+          {UPCOMMING_MEETINGS.map((item) => {
+            return <div className='meeting' key={item.id}>
+              <div className="meeting-main">
+                <h3>{item.title}</h3>
+                <p>Host: {item.host}</p>
+              </div>
+              <div className="meeting-description">
+                <p className="meeting-time">
+                  {item.time}
+                </p>
+                <div className="join-btn">
+                  <button>Join</button>
+                </div>
+              </div>
+
+            </div>
+          })}
+        </div>
+
       </section>
 
     </main>
