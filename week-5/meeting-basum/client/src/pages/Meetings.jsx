6d47cf452b365bd78cba61b33c0e18274ffb1d7c
chii-beep <christinaxtha6@gmail.com>
2026-06-21 13:57:17 +0545

    Chore: Docker Configured

--- week-5/meeting-basum/client/src/pages/Meetings.jsx
+++ week-5/meeting-basum/client/src/pages/Meetings.jsx
@@ -1,24 +1,31 @@
 import { useMeetingList } from "../store/meetingStore"
 
 const Meetings = () => {
-   const meetings = useMeetingList((state) => state.meetings)
-   const count = meetings.length
+  const meetings = useMeetingList((state) => state.meetings)
+  const count = meetings.length
+  const addMeeting = useMeetingList(s => s.addMeeting)
+  // addMeeting("Meeting 4")
 
   return (
-   <div>
-          List of Meetings: 
-        {
-          // console.log(meetings)
-          meetings.map((item, index) => {
-            return <li key={index}>{item}</li>
-          })
-        }
+    <div>
+      List of Meetings:
+      {
+        // console.log(meetings)
+        meetings.map((item, index) => {
+          return <li key={index}>{item}</li>
+        })
+      }
 
-        Total Meetings: 
-        {
-          count
-        }
-        </div>
+      Total Meetings:
+      {
+        count
+      }
+
+      <button onClick={() => addMeeting(`Meeting ${meetings.length + 1}`)}>
+        Add Meeting
+      </button>
+
+    </div>
 
   )
 }
--- week-5/meeting-basum/client/src/store/meetingStore.js
+++ week-5/meeting-basum/client/src/store/meetingStore.js
@@ -3,6 +3,11 @@ import {create} from "zustand"
 export const useMeetingList = create((set, get)=> ({
     meetings: ["Meeting 1", "Meeting 2", "Meeting 3"],
     getMeetings: () => state.meetings,
-    addMeetings: (value) => set((state) => [...state.meetings, value]),
+    // addMeetings: (value) => set((state) => [...state.meetings, value]),
+    addMeeting: (value) => {
+        set((state) => ({
+            meetings: [...state.meetings, value]
+        }))
+    }
 
 }))
